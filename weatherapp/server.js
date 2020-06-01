b1ec21016147755626e154d612cdc1a54949748c
aakashDeveloper <ahanda205@gmail.com>
2020-06-01 08:28:03 +0530

    a

--- weatherapp/server.js
+++ weatherapp/server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var request = require('request');
-var app = express();
-var port = process.env.port || 7800;
+import express from 'express';
+import request from 'request';
+const app = express();
+const port = process.env.port || 7800;
 
-var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=mumbai&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
+const apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=mumbai&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
 
 //Static files
 app.use(express.static(__dirname+'/public'));
@@ -12,16 +12,15 @@ app.set('views', './src/views');
 //View engine
 app.set('view engine','ejs');
 
-app.get('/weather',function(req,res){
-    request(apiUrl,function(err,response){
+app.get('/weather',(req,res) => {
+    request(apiUrl,(err,response)=>{
         if(err) throw err;
         const output = JSON.parse(response.body)
-        //res.send(output.list)
         res.render('weather',{title:'Weather app',result:output})
     })
 })
 
-app.listen(port,function(err){
+app.listen(port,(err)=>{
     if(err) throw err;
     console.log(`Server is running on port ${port}`)
 })
